26afef2ac2cad32018bbdb32c71c829c538b98a6
james.chan <james.chan@ijoyful.com>
2023-10-05 11:16:56 +0800

    初始化

--- app/src/main/java/com/hello/MainActivity.kt
+++ app/src/main/java/com/hello/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.hello
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.hello.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +10,17 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    private val viewModel: MainViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.button.setOnClickListener{
+            binding.textView.text = "button clicked"
+        }
+
+        binding.button2.setOnClickListener{
+            binding.textView.text = "button2 clicked"
+        }
     }
 }
