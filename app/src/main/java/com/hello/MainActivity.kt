c865f1afe57ff32966d28ab82d4d4dfa4dbf244b
james.chan <james.chan@ijoyful.com>
2023-10-06 17:38:08 +0800

    新增 libraries - hilt

--- app/src/main/java/com/hello/MainActivity.kt
+++ app/src/main/java/com/hello/MainActivity.kt
@@ -1,25 +1,29 @@
 package com.hello
 
 import android.os.Bundle
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.hello.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
-    private val viewModel: MainViewModel by viewModels()
+    @Inject
+    lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.button.setOnClickListener{
-            binding.textView.text = "button clicked"
+        binding.button.setOnClickListener {
+            binding.textView.text = viewModel.sayHello()
         }
 
-        binding.button2.setOnClickListener{
+        binding.button2.setOnClickListener {
             binding.textView.text = "button2 clicked"
         }
     }
--- app/src/main/java/com/hello/MainViewModel.kt
+++ app/src/main/java/com/hello/MainViewModel.kt
@@ -1,6 +1,13 @@
 package com.hello
 
 import androidx.lifecycle.ViewModel
+import javax.inject.Inject
 
-class MainViewModel(): ViewModel() {
+class MainViewModel @Inject constructor() : ViewModel() {
+    init {
+        println("MainViewMode created")
+    }
+
+    fun sayHello(): String = "hello"
+
 }
